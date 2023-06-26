9fd1b0c7b03a2e8221e46ae92b3f777531a539a5
Huynh.nguyen <huynh.nguyen@amela.vn>
2023-06-26 21:36:19 +0700

    three and for

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import LatestBlogposts1 from "./components/Blogs/LatestBlogposts1";
 import LatestBlogposts2 from "./components/Blogs/LatestBlogposts2";
 import OurAwesomeTeam1 from "./components/Teams/OurAwesomeTeam1";
 import OurAwesomeTeam2 from "./components/Teams/OurAwesomeTeam2";
+import LatestBlogposts3 from "./components/Blogs/LatestBlogposts3";
+import LatestBlogposts4 from "./components/Blogs/LatestBlogposts4";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <LatestBlogposts1 />
       <LatestBlogposts2 />
+      <LatestBlogposts3 />
+      <LatestBlogposts4 />
       <OurAwesomeTeam1 />
       <OurAwesomeTeam2 />
     </div>
--- src/components/Blogs/style.js
+++ src/components/Blogs/style.js
@@ -143,3 +143,107 @@ export const BlogItemPostWrapper = styled.div`
     text-decoration: underline !important;
   }
 `;
+
+export const LatestBlogposts3ItemWrapper = styled.div`
+  display: flex;
+  margin: 10px 0;
+  img {
+    width: 286px;
+    height: 191px;
+    box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.3);
+    border-radius: 0.1875rem;
+    margin-right: 20px;
+  }
+  h3 {
+    border-bottom: 2px solid transparent;
+    color: #444;
+    font-weight: 400;
+    font-size: 1.7rem;
+    cursor: pointer;
+    margin: 5px 0;
+  }
+  h3:hover {
+    text-decoration: underline;
+  }
+  p {
+    color: #9a9a9a;
+    font-weight: 300;
+    line-height: 1.61em;
+    font-size: 1.2em;
+    margin-top: 0;
+  }
+  .author {
+    display: flex;
+    align-items: center;
+    img {
+      width: 30px;
+      height: 30px;
+      overflow: hidden;
+      border-radius: 50%;
+      margin-right: 5px;
+      box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.3);
+    }
+  }
+`;
+
+export const BlogItem4PostWrapper = styled.div`
+  max-width: 700px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .title {
+    width: 700px;
+    font-weight: 700;
+    padding-top: 30px;
+    margin-bottom: 30px;
+    font-size: 2.5em;
+  }
+`;
+export const BlogItem4PostItemWrapper = styled.div`
+  text-align: center;
+  margin-bottom: 60px;
+  img {
+    width: 100%;
+    box-shadow: 0 10px 25px 0 rgba(0, 0, 0, 0.3);
+    border-radius: 0.1875rem;
+  }
+  h6 {
+    color: #2ca8ff;
+    margin-top: 1rem;
+    margin: 10px 0;
+  }
+  h3 {
+    border-bottom: 2px solid transparent;
+    color: #444;
+    font-weight: 400;
+    font-size: 1.7rem;
+    cursor: pointer;
+    margin: 5px 0;
+  }
+  h3:hover {
+    text-decoration: underline;
+  }
+  p {
+    color: #9a9a9a;
+    font-weight: 300;
+    line-height: 1.61em;
+    font-size: 1.2em;
+    margin-top: 0;
+  }
+  .btn-read-more {
+    border-width: 1px;
+    border-radius: 30px !important;
+    padding: 11px 23px;
+    background-color: #2ca8ff;
+    color: #fff;
+    font-weight: 400;
+    font-size: 0.8571em;
+    line-height: 1.35em;
+    margin: 10px 1px;
+    border: none;
+    cursor: pointer;
+    width: 130px;
+    margin: 0 auto;
+  }
+`;
