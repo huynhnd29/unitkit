2e36acb46cb9244b19af9fdec970ae92eebbea70
quynhtran0397 <quynhtran0397@gmail.com>
2023-06-29 11:32:34 +0700

    awesom 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Header from "./components/Header/Header";
-import LatestBlogposts1 from "./components/Blogs/LatestBlogposts1";
-import LatestBlogposts2 from "./components/Blogs/LatestBlogposts2";
-import OurAwesomeTeam1 from "./components/Teams/OurAwesomeTeam1";
-import OurAwesomeTeam2 from "./components/Teams/OurAwesomeTeam2";
-import OurAwesomeTeam3 from "./components/Teams/OurAwesomeTeam3";
-import LatestBlogposts3 from "./components/Blogs/LatestBlogposts3";
-import LatestBlogposts4 from "./components/Blogs/LatestBlogposts4";
-import LatestBlogposts5 from "./components/Blogs/LatestBlogposts5";
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import LatestBlogposts1 from './components/Blogs/LatestBlogposts1';
+import LatestBlogposts2 from './components/Blogs/LatestBlogposts2';
+import OurAwesomeTeam1 from './components/Teams/OurAwesomeTeam1';
+import OurAwesomeTeam2 from './components/Teams/OurAwesomeTeam2';
+import OurAwesomeTeam3 from './components/Teams/OurAwesomeTeam3';
+import LatestBlogposts3 from './components/Blogs/LatestBlogposts3';
+import LatestBlogposts4 from './components/Blogs/LatestBlogposts4';
+import LatestBlogposts5 from './components/Blogs/LatestBlogposts5';
+import OurAwesomeTeam4 from './components/Teams/OurAwesomeTeam4';
+import OurAwesomeTeam5 from './components/Teams/OurAwesomeTeam5';
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
       <OurAwesomeTeam1 />
       <OurAwesomeTeam2 />
       <OurAwesomeTeam3 />
+      <OurAwesomeTeam4 />
+      <OurAwesomeTeam5 />
     </div>
   );
 }
